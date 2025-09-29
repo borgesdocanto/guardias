2378ee1d4205aa301686b7adf1f2bb6c9bf31d3e
Leandro Borges Do Canto <leandroborges@hotmail.com>
2025-09-29 18:58:15 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GuardiasApp from "./GuardiasApp";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <GuardiasApp />
+    </div>
+  );
+}
+
+export default App;
